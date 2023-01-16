770c1b31a362c66a7169c43d0925f920fcf75b4b
JunqiDu <50341989+JunqiDu@users.noreply.github.com>
2023-01-16 15:36:42 -0500

    change as aut test

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,5 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log('THEY ARE SAME!');
-  } else {
-    console.log('They are different!');
-  }
-};
+const assertEqual = function(actual, expected) {
+  // ...
+}
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-const name = 'Joseph';
-console.log(`Hello, ${name}!`);
+module.exports = assertEqual;
